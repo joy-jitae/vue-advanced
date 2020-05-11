f2c9590f4ef5ba87acca82e66b82abf8fc662b66
jhyu <jhyu@wemb.co.kr>
2020-05-12 08:03:44 +0900

    actions와 mutations 속성 모듈화

---
+++ src/store/actions.js
@@ -0,0 +1,30 @@
+import {fetchAskList, fetchJobsList, fetchNewsList} from "../api";
+
+export default {
+	FETCH_NEWS(context) {
+		fetchNewsList().then(response => {
+			console.log(response.data);
+			context.commit('SET_NEWS', response.data);
+		}).catch(error => {
+			console.log(error);
+		})
+	},
+	FETCH_JOBS({commit}) {
+		// context 안에 인자로 commit 이 있음
+		fetchJobsList().then(({data}) => {
+			commit('SET_JOBS', data);
+		})
+			.catch(error => {
+				console.log(error);
+			});
+	},
+	FETCH_ASKS({commit}) {
+		fetchAskList()
+			.then(({data}) => {
+				commit('SET_ASKS', data);
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+}
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import Vue from 'vue';
 
 import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
 
+import mutations from './mutations.js';
+import actions from './actions.js';
+
 
 Vue.use(Vuex);
 
@@ -18,43 +21,6 @@ export const store = new Vuex.Store({
           return state.ask;
       }
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-        SET_ASKS(state, asks) {
-            state.ask = asks
-        }
-    },
-    actions : {
-        FETCH_NEWS(context) {
-            fetchNewsList().then(response => {
-                console.log(response.data);
-                context.commit('SET_NEWS', response.data);
-            }).catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_JOBS({commit}) {
-            // context 안에 인자로 commit 이 있음
-            fetchJobsList().then(({data}) => {
-                commit('SET_JOBS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        FETCH_ASKS({commit}) {
-            fetchAskList()
-                .then(({data}) => {
-                    commit('SET_ASKS', data);
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        }
-    }
+    mutations,
+    actions
 })
